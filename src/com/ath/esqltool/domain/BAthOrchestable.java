7c062109909ed31afd64109b4c64d68f70a9f066
milton.vega <milton.vega@PE-HO-070.PERIFERIAGROUP.MS>
2017-08-28 10:42:59 -0500

    Soporte de especificos

---
+++ src/com/ath/esqltool/domain/BAthOrchestable.java
@@ -0,0 +1,12 @@
+package com.ath.esqltool.domain;
+
+public interface BAthOrchestable {
+	
+	
+	public String getId();
+	public String getName();
+	public String getInputMq();
+	public String getType();
+	public String getLongDescription();
+	
+}
---
+++ src/com/ath/esqltool/domain/BAthSpecificBo.java
@@ -0,0 +1,97 @@
+package com.ath.esqltool.domain;
+
+public class BAthSpecificBo implements BAthOrchestable {
+
+	private String id;
+	private String name;
+	private String inputMq;
+	private String type; 
+	
+	private Boolean passthrough;
+
+	private String bankOrg;
+	private String codService;
+	private String bankId;
+	
+	public BAthSpecificBo() {
+		passthrough = true;
+	}
+	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInputMq() {
+		return inputMq;
+	}
+
+	public void setInputMq(String inputMq) {
+		this.inputMq = inputMq;
+	}
+
+	public String getType() {
+		return "Specific Passthrough";
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getLongDescription() {
+		StringBuffer buffer = new StringBuffer();
+		
+		buffer.append(getType());
+		buffer.append("|");
+		buffer.append(getName() + "Svc_");
+		buffer.append(getBankOrg());
+		//CNTL|"+ combocntls.getText()
+		return buffer.toString();
+	}
+
+
+	public String getBankOrg() {
+		return bankOrg;
+	}
+
+	public void setBankOrg(String bankOrg) {
+		this.bankOrg = bankOrg;
+	}
+
+	public String getCodService() {
+		return codService;
+	}
+
+	public void setCodService(String codService) {
+		this.codService = codService;
+	}
+
+	public String getBankId() {
+		return bankId;
+	}
+
+	public void setBankId(String bankId) {
+		this.bankId = bankId;
+	}
+
+	public Boolean getPassthrough() {
+		return passthrough;
+	}
+
+	public void setPassthrough(Boolean passthrough) {
+		this.passthrough = passthrough;
+	}
+
+}
